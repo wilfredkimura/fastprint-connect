5ffd7a9f7ef2b125f3fb8ea08a559f898ff6b195
wilfredkimura <wilfredmutahi9@gmail.com>
2025-10-23 23:42:41 +0300

    index.js fix

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import serverless from 'serverless-http';
 import app from '../server/app.js';
 
 export const config = {
-  runtime: 'nodejs18.x',
+  runtime: 'nodejs',
 };
 
 export default serverless(app);
